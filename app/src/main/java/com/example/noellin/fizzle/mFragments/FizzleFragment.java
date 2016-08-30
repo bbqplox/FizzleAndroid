929cfceac46e74ef890ff8a325ed13fac1bc5b28
Joseph Widjaja <jewidjaj@ucsd.edu>
2016-08-29 22:07:11 -0700

    nice inventory and fizzle popup

--- app/src/main/java/com/example/noellin/fizzle/mFragments/FizzleFragment.java
+++ app/src/main/java/com/example/noellin/fizzle/mFragments/FizzleFragment.java
@@ -1,7 +1,7 @@
 package com.example.noellin.fizzle.mFragments;
 
 /**
- * Created by noellin on 8/27/16.
+ * Created by Joseph Widjaja on 8/27/16.
  */
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -17,21 +17,32 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.view.Window;
+import android.app.Dialog;
 
 import com.example.noellin.fizzle.R;
+
+import java.util.ArrayList;
 
 public class FizzleFragment extends Fragment {
 
     private Spinner spinner;
     private Spinner spinner2;
     private Spinner spinner3;
-    private static final String[]paths = {"select item","item 1", "item 2", "item 3"};
+    private static final String[] paths = {"select item","item 1", "item 2", "item 3", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test"};
+    final ArrayList<String> selected = new ArrayList<String>();
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fizzle_fragment,container,false);
+        getActivity().setTitle("Fizzle");
+
+
+        for(int i = 0; i < 3; i++) {
+            selected.add("");
+        }
 
         spinner = (Spinner)rootView.findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
@@ -55,16 +66,29 @@ public class FizzleFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter3);
 
-        final String text = spinner.getSelectedItem().toString();
-        final String text2 = spinner.getSelectedItem().toString();
-        final String texgt3 = spinner.getSelectedItem().toString();
-    
         Button button =(Button)rootView.findViewById(R.id.button);
         button.setOnClickListener(new OnClickListener()
         {
             @Override
             public void onClick(View v){
-                Toast.makeText(getContext(), "Fizzle is clicked", Toast.LENGTH_SHORT).show();
+                final String text = spinner.getSelectedItem().toString();
+                final String text2 = spinner2.getSelectedItem().toString();
+                final String text3 = spinner3.getSelectedItem().toString();
+
+                selected.set(0, text);
+                selected.set(1, text2);
+                selected.set(2, text3);
+
+                Dialog settingsDialog = new Dialog(getActivity());
+                settingsDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+                settingsDialog.setContentView(getActivity().getLayoutInflater().inflate(R.layout.fizzle_popup
+                        , null));
+                settingsDialog.show();
+
+
+
+
+                Toast.makeText(getContext(), "Fizzle with " + selected.get(0) + " " + selected.get(1) + " " + selected.get(2) , Toast.LENGTH_SHORT).show();
             }
         });
 
